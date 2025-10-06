d86300eeeee432f9453b2107f941a95480f5cc31
vharini24 <vharini592@gmail.com>
2025-10-06 19:49:46 +0530

    Update views.py

--- Restaurant/webpage/views.py
+++ Restaurant/webpage/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def web(request):
+    return render(request,'index.html')
+
+def menu(request):
+    return render(request,'menu.html')
+
+def admins(request):
+    return render(request,'admin.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def booktable(request):
+    return render(request,'booktable.html')
+
